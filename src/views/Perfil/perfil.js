982a4bd2abf11e9e3a8c0b8780af76b161284f2e
Sérgio Segaty <sergio.segaty@gmail.com>
2020-08-27 16:49:31 -0300

    Adicionado View de Perfil.

--- src/views/Perfil/perfil.js
+++ src/views/Perfil/perfil.js
@@ -1,29 +1,48 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components";
+import NovaTransferencia from "./novaTransferencia";
+import Historico from "./historico";
 
-const NameContainer = styled.View`
+const PerfilContainer = styled.View`
   margin-top: 30px;
   align-items: center;
   border-bottom-right-radius: 16px;
   border-bottom-left-radius: 16px;
   background-color: #95C285;
-  elevation: 10;
   width: 94%;
   align-self: center;
+  height: 75px;
+  justify-content: center;
+  elevation: 20;
 
 `;
 
 const Name = styled.Text`
-font-size: 35px;
+font-size: 30px;
+font-weight: bold;
+`
+
+const ProfilePicture = styled.Image`
+margin-top: 25px;
+border-radius: 120px;
+height: 200px;
+width: 200px;
+align-self: center;
 `
 
 function Perfil(props) {
   return (
     <>
-      <NameContainer>
-        <Name> Agatha Waillie </Name>
-      </NameContainer>
+      <PerfilContainer>
+        <Name> Lanna Souza </Name>
+        </PerfilContainer>
+        <ProfilePicture
+        source={require('../../../assets/profilePics/lanna-souza.jpg')}/>
+
+        <NovaTransferencia/>
+
+        <Historico/>
     </>
   );
 }
