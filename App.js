e2b5ab11d2b011e5abdfc21d0444c44d74244e2e
Sérgio Segaty <sergio.segaty@gmail.com>
2020-08-27 16:11:18 -0300

    Alterado AppShell

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/views/Login/login';
 import Home from './src/views/Home/home';
+import Extrato from './src/views/Extrato/extrato';
+import Perfil from './src/views/Perfil/perfil';
 
 
 export default function App() {
   return (
-    <View>
+    <>
       <StatusBar/>
-      <Home/>
-    </View>
+      <Perfil/>
+    </>
   );
 }
